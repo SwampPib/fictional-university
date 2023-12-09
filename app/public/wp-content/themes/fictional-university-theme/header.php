ecbb4e31d4941ab7ca5137860703b74b467aaa1b
Pippa <pippa@kika.ca>
2023-12-08 19:17:12 -0800

    Section 8:36 content relationship

--- app/public/wp-content/themes/fictional-university-theme/header.php
+++ app/public/wp-content/themes/fictional-university-theme/header.php
@@ -21,7 +21,7 @@
             <!--<ul> This chunk is custom URL styling options for mainMenu and menu child pages
               <li <?php if(is_page('about-us') or wp_get_post_parent_id(0) == 10) echo 'class="current-menu-item"' ?>><a href="<?php echo site_url('/about-us') ?>">About Us</a></li>
               <li><a href="#">Programs</a></li>
-              <li><a href="#">Events</a></li>
+              <li <?php if (get_post_type() == 'event' OR is_page('past_events')) echo 'class="current-menu-item"'; ?>><a href="<?php echo get_post_type_archive_link('event'); ?>">Events</a></li>
               <li><a href="#">Campuses</a></li>
               <li><?php if (get_post_type() == 'post') echo 'class="current-menu-item"'; ?><a href="<?php echo site_url('/blog') ?>">Blog</a></li>
             </ul>-->
