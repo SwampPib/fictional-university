6b9f81408cb149a8ac06fb6ae838155cf7c75af6
Pippa <pippa@kika.ca>
2023-12-19 16:22:02 -0800

    section 10:49 remove redundancy

--- app/public/wp-content/themes/fictional-university-theme/page-past-events.php
+++ app/public/wp-content/themes/fictional-university-theme/page-past-events.php
@@ -1,6 +1,6 @@
 <?php
 
-get_header(); 
+get_header();
 pageBanner(array(
   'title' => 'Past Events',
   'subtitle' => 'A recap of our past events.'
@@ -8,47 +8,33 @@ pageBanner(array(
 ?>
 
 <div class="container container--narrow page-section">
-    <?php
+  <?php
 
-$today = date('Ymd');
-      $pastEvents = new WP_Query(array(
-        'paged' => get_query_var('paged', 1),
-        'post_type' => 'event',
-        'order_by' => 'meta_value',
-        'meta_key' => 'event_date',
-        'order' => 'ASC',
-        'meta_query' => array(
-          array(
-            'key' => 'event_date',
-            'compare' => '<',
-            'value' => $today,
-            'type' => 'numeric'
-          )
-        )
-      ));
+  $today = date('Ymd');
+  $pastEvents = new WP_Query(array(
+    'paged' => get_query_var('paged', 1),
+    'post_type' => 'event',
+    'order_by' => 'meta_value',
+    'meta_key' => 'event_date',
+    'order' => 'ASC',
+    'meta_query' => array(
+      array(
+        'key' => 'event_date',
+        'compare' => '<',
+        'value' => $today,
+        'type' => 'numeric'
+      )
+    )
+  ));
 
-    while ($pastEvents->have_posts()) {
-        $pastEvents->the_post(); ?>
-        <div class="event-summary">
-        <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-            <span class="event-summary__month"><?php 
-            $eventDate = new DateTime(get_field('event_date'));
-            echo $eventDate->format('M')
-            ?></span>
-            <span class="event-summary__day"><?php 
-            echo $eventDate->format('d')
-            ?></span>
-          </a>
-            <div class="event-summary__content">
-                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
-                <p><?php echo wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Learn more</a></p>
-            </div>
-        </div>
-    <?php }
-    echo paginate_links(array(
-         'total' => $pastEvents->max_num_pages
-    ));
-    ?>
+  while ($pastEvents->have_posts()) {
+    $pastEvents->the_post();
+    get_template_part('template-parts/content-event');
+  }
+  echo paginate_links(array(
+    'total' => $pastEvents->max_num_pages
+  ));
+  ?>
 </div>
 
 <?php get_footer();
